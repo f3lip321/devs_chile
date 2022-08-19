bdfc28b3e454e16df8a1abc9c21a1bdd7b751344
unknown <catalan.allendes@gmail.com>
2022-08-18 22:44:50 -0400

    first commit

---
+++ routes/answers.js
@@ -0,0 +1,48 @@
+var express = require('express');
+const axios = require('axios')
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+
+  try {
+    const response = await axios.get("https://sueldos-tech-chile-2022.herokuapp.com/v2/answers")
+    const answers = response.data
+    const metadataKeys = [
+      "Submission ID",
+    "Respondent ID",
+    "Submitted at"
+    ]
+
+    const newResponse = answers
+      .map((answer) => {
+        let newAnswer = {
+          answers: []
+        }
+
+        for(key in answer){
+          if (metadataKeys.includes(key)){
+            newAnswer = {              
+              [key]: answer[key],
+              ...newAnswer
+            }
+          }
+            else{
+              newAnswer.answers.push({
+                question: key,
+                answer: answer[key]
+              })
+            }
+          } 
+          
+          return newAnswer
+      })
+    
+    res.send(newResponse);  
+  } catch (error) {
+    res.send(error.message)
+  }
+  
+});
+
+module.exports = router;
---
+++ tests/answers.test.js
@@ -0,0 +1,26 @@
+const supertest = require('supertest')
+const app = require('../app')
+const expectedResponse = require('./fixtures/answers-expected.json')
+
+jest.mock('axios', () => {
+    const fixedResponse = require('./fixtures/answers.json')
+    return{
+        get(url){
+            return Promise.resolve({
+                data: fixedResponse
+            })
+        }
+    }
+})
+
+describe("/answers Endpoint", () => {
+
+    it("GET /answers", async() => {
+        const response = await supertest(app)
+            .get('/answers')
+            
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(expectedResponse)
+    })
+
+})
