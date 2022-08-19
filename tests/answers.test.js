ddc43dd2e18221a51c6fbfb7c64c465f1c7c795f
unknown <catalan.allendes@gmail.com>
2022-08-18 23:39:14 -0400

    chore(workflows): updating workflow adding jest and eslint

--- tests/answers.test.js
+++ tests/answers.test.js
@@ -3,24 +3,22 @@ const app = require('../app')
 const expectedResponse = require('./fixtures/answers-expected.json')
 
 jest.mock('axios', () => {
-    const fixedResponse = require('./fixtures/answers.json')
-    return{
-        get(url){
-            return Promise.resolve({
-                data: fixedResponse
-            })
-        }
+  const fixedResponse = require('./fixtures/answers.json')
+  return {
+    get (url) {
+      return Promise.resolve({
+        data: fixedResponse
+      })
     }
+  }
 })
 
-describe("/answers Endpoint", () => {
+describe('/answers Endpoint', () => {
+  it('GET /answers', async () => {
+    const response = await supertest(app)
+      .get('/answers')
 
-    it("GET /answers", async() => {
-        const response = await supertest(app)
-            .get('/answers')
-            
-        expect(response.status).toBe(200)
-        expect(response.body).toEqual(expectedResponse)
-    })
-
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual(expectedResponse)
+  })
 })
